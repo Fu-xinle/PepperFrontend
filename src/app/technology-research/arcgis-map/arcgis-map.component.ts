c7d70ed3cef4356493e65895d7090c8335b75511
FuXinLe <2848986563@qq.com>
2022-01-25 17:10:30 +0800

    接入 arcgis js api

--- src/app/technology-research/arcgis-map/arcgis-map.component.ts
+++ src/app/technology-research/arcgis-map/arcgis-map.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+
+import Map from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
 
 @Component({
   selector: 'app-arcgis-map',
   templateUrl: './arcgis-map.component.html',
   styleUrls: ['./arcgis-map.component.scss'],
 })
-export class ArcgisMapComponent implements OnInit {
-  constructor() {}
+export class ArcgisMapComponent implements OnInit, OnDestroy {
+  @ViewChild('mapViewNode', { static: true }) private mapViewEl!: ElementRef;
+  public view: any = null;
 
-  ngOnInit(): void {
-    console.info(1);
+  initializeMap() {
+    const container = this.mapViewEl.nativeElement;
+
+    const map = new Map({
+      basemap: 'arcgis-topographic',
+    });
+
+    this.view = new MapView({
+      container,
+      map: map,
+      center: [-118.244, 34.052],
+      zoom: 12,
+    });
+  }
+
+  ngOnInit(): any {
+    // Initialize MapView and return an instance of MapView
+    this.initializeMap();
+  }
+
+  ngOnDestroy(): void {
+    if (this.view) {
+      this.view.destroy();
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import esriConfig from '@arcgis/core/config.js';
 import { LicenseManager } from 'ag-grid-enterprise';
 
 import { AppModule } from './app/app.module';
@@ -8,6 +9,10 @@ import { environment } from './environments/environment';
 
 // ag-grid-enterprise 企业版本序列号
 LicenseManager.setLicenseKey('OTQ1MzA4OTQ1Njk3Mw==390bb30c7ca829b2dcbef7b197f8d33e');
+
+// arcgis-js-api的key值，用于访问arcgis-online上的服务
+esriConfig.apiKey = 'AAPKc626e9f806e742b8979ed64af99c90883T96vc-6fQBPHFxXdxTWiFIdEvxSgwPXHMafJwu6RNyTjAJKpmsHMWNtzA_gTJNr';
+esriConfig.assetsPath = '/assets/arcgis-js';
 
 if (environment.production) {
   enableProdMode();
